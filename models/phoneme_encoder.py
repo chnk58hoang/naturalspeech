28915e278781eced3499be49793b688a321ab2c3
chnk58hoang <19021285@vnu.edu.vn>
2024-07-29 17:51:57 +0700

    add relative emb

--- models/phoneme_encoder.py
+++ models/phoneme_encoder.py
@@ -35,39 +35,53 @@ class FeedForwardNetwork(nn.Module):
 class RelativeMultiHeadAttention(nn.Module):
     def __init__(self,
                  num_heads: int,
-                 in_channels: int,
                  out_channels: int,
-                 hidden_channels) -> None:
+                 hidden_channels: int,
+                 p_dropout: float,
+                 relative_window_size: int = None) -> None:
         super().__init__()
-        assert in_channels % num_heads == 0, "channels should be divisible by num_heads"
+        assert hidden_channels % num_heads == 0, "channels should be divisible by num_heads"
         self.num_heads = num_heads
-        self.in_channels = in_channels
         self.out_channels = out_channels
         self.hidden_channels = hidden_channels
-        self.convq = nn.Conv1d(in_channels=in_channels,
+        self.relative_window_size = relative_window_size
+        self.head_channels = hidden_channels // num_heads
+        self.convq = nn.Conv1d(in_channels=hidden_channels,
                                out_channels=hidden_channels,
                                kernel_size=1)
-        self.convk = nn.Conv1d(in_channels=in_channels,
+        self.convk = nn.Conv1d(in_channels=hidden_channels,
                                out_channels=hidden_channels,
                                kernel_size=1)
-        self.convv = nn.Conv1d(in_channels=in_channels,
+        self.convv = nn.Conv1d(in_channels=hidden_channels,
                                out_channels=hidden_channels,
                                kernel_size=1)
+        self.dropout = nn.Dropout(p_dropout)
+        if self.relative_window_size is not None:
+            rel_dev = self.head_channels ** -0.5
+            emb_rel_k = nn.Parameter(torch.randn(self.num_heads,
+                                                 self.relative_window_size * 2 + 1,
+                                                 self.head_channels) * rel_dev)
+            emb_rel_v = nn.Parameter(torch.randn(self.num_heads,
+                                                 self.relative_window_size * 2 + 1,
+                                                 self.head_channels) * rel_dev)
+            self.register_parameter(name='emb_rel_k', param=emb_rel_k)
+            self.register_parameter(name='emb_rel_v', param=emb_rel_v)
 
     def attention(self, q, k, v):
         """
         q, k, v: tensor (B, C, L)
         """
-        head_channels = self.hidden_channels // self.num_heads
-        batch, channel, length = q.size()
+        head_channels = self.head_channels
+        batch, _, length = q.size()
         q = q.view(batch, self.num_heads, head_channels, length).transpose(2, 3)
         k = k.view(batch, self.num_heads, head_channels, length).transpose(2, 3)
         v = v.view(batch, self.num_heads, head_channels, length).transpose(2, 3)
         score = torch.matmul(q, k.transpose(2, 3)) / math.sqrt(head_channels)
         score = F.softmax(score, -1)
+        score = self.dropout(score)
         res = torch.matmul(score, v)  # batch, num_head, length, head_channels
         res = res.tranpose(2, 3).contigous().view(batch, self.hidden_channels, length)
-        
+        return res
 
     def forward(self, x):
         pass
