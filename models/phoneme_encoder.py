cd9169f637685166d21d3456b5562c6b069cb23a
chnk58hoang <19021285@vnu.edu.vn>
2024-07-29 13:36:39 +0700

    add attention forward

--- models/phoneme_encoder.py
+++ models/phoneme_encoder.py
@@ -69,6 +69,5 @@ class RelativeMultiHeadAttention(nn.Module):
         res = res.tranpose(2, 3).contigous().view(batch, self.hidden_channels, length)
 
 
-
-
-
+    def forward(self, x):
+        pass
